0738f514ce5e9f5bffeb9b72f541ca2c57599077
kolberg-dev <74478295+kolberg-dev@users.noreply.github.com>
2022-02-11 11:13:26 +0100

    Refactored submit handler

--- main.go
+++ main.go
@@ -25,51 +25,11 @@ func main() {
 	}
 
 	submitHandler := func() {
-		alphabet := "abcdefghijklmnopqrstuvwxyz"
 		wrongLetters := wrongLettersInput.GetText()
-		validLetters := ""
-		for _, l := range alphabet {
-			if !strings.Contains(wrongLetters, string(l)) {
-				validLetters += string(l)
-			}
-		}
-		rxString := ""
 		correct := getFormInputs(*correctLetters)
 		wrongBoxLetters := getFormInputs(*wrongBox)
-		for i, l := range correct {
-			if len(l) == 0 {
-				if len(wrongBoxLetters[i]) > 0 {
-					validSub := ""
-					for _, d := range validLetters {
-						if !strings.Contains(wrongBoxLetters[i], string(d)) {
-							validSub += string(d)
-						}
-					}
-					rxString += "[" + validSub + "]"
-				} else {
-					rxString += "[" + validLetters + "]"
-				}
-			} else {
-				rxString += string(l)
-			}
-		}
-		rx := regexp.MustCompile(rxString)
-		combined := strings.Join(wrongBoxLetters, "")
-		rx2 := regexp.MustCompile(".")
-		if len(combined) > 0 {
-			rx2 = regexp.MustCompile("[" + combined + "]")
-		}
-		words := readWordlist()
-		remainingWords := ""
-		for _, w := range words {
-			if rx.MatchString(w) && rx2.MatchString(w) {
-				if len(remainingWords) > 0 {
-					remainingWords += " "
-				}
-				remainingWords += w
-			}
-		}
-		setWords(remainingWords)
+		words := getWords(wrongLetters, correct, wrongBoxLetters)
+		setWords(words)
 	}
 
 	reset.SetSelectedFunc(func() { setWords("reset") })
@@ -93,6 +53,51 @@ func main() {
 	if err := app.SetRoot(flex, true).SetFocus(flex).Run(); err != nil {
 		panic(err)
 	}
+}
+
+func getWords(wrongLetters string, correct []string, wrongBoxLetters []string) string {
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+	validLetters := ""
+	for _, l := range alphabet {
+		if !strings.Contains(wrongLetters, string(l)) {
+			validLetters += string(l)
+		}
+	}
+	rxString := ""
+	for i, l := range correct {
+		if len(l) == 0 {
+			if len(wrongBoxLetters[i]) > 0 {
+				validSub := ""
+				for _, d := range validLetters {
+					if !strings.Contains(wrongBoxLetters[i], string(d)) {
+						validSub += string(d)
+					}
+				}
+				rxString += "[" + validSub + "]"
+			} else {
+				rxString += "[" + validLetters + "]"
+			}
+		} else {
+			rxString += string(l)
+		}
+	}
+	rx := regexp.MustCompile(rxString)
+	combined := strings.Join(wrongBoxLetters, "")
+	rx2 := regexp.MustCompile(".")
+	if len(combined) > 0 {
+		rx2 = regexp.MustCompile("[" + combined + "]")
+	}
+	words := readWordlist()
+	remainingWords := ""
+	for _, w := range words {
+		if rx.MatchString(w) && rx2.MatchString(w) {
+			if len(remainingWords) > 0 {
+				remainingWords += " "
+			}
+			remainingWords += w
+		}
+	}
+	return remainingWords
 }
 
 func readWordlist() []string {
